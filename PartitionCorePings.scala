639b9d637ad011cce775b9614e7f46272ae7f62a
Erin Cole <ecole@mozilla.com>
2018-07-17 16:26:44 -0700

    include scala benchmark tests

---
+++ PartitionCorePings.scala
@@ -0,0 +1,44 @@
+/*
+ * This program is a benchmark test for the partition behavior of running Spark Jobs on a cluster. It uses 6
+ * Parititions and the data for Core Pings.
+ */
+
+package com.mozilla.telemetry
+
+import com.mozilla.telemetry.heka.{Dataset}
+import org.apache.spark.sql.SparkSession
+
+object PartitionCorePings {
+  def main(args: Array[String]) {
+    val startTime = System.currentTimeMillis()
+    val spark = SparkSession.builder.appName("ecole").getOrCreate()
+    implicit val sc = spark.sparkContext
+    // specifcation for partition behavior
+    val numPartitions = Some(6)
+
+    println()
+    println("Testing performance with Partition Algorithm and Core Pings data...")
+
+    val fs_rdd = {
+      Dataset("telemetry")
+        .where("sourceName") {
+          case "telemetry" => true
+        }
+        .where("docType") {
+          case "core" => true
+        }
+        .where("appUpdateChannel") {
+          case "nightly" => true
+        }
+        .records(numPartitions)
+    }
+
+    fs_rdd.first().fields
+    sc.stop()
+    val endTime = System.currentTimeMillis()
+
+    //used to measure peformance in terms of runtime of program
+    printf("Test ran in %d seconds", (endTime - startTime)/1000)
+    println()
+  }
+}
