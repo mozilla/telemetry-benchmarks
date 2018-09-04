bbed183014b0aef84fb3865808e4ec6dab09b349
Erin Cole <ecole@mozilla.com>
2018-09-04 15:48:43 -0700

    include correct Python file

--- moztelemetry/partition-packing/PartitionPerformance.py
+++ moztelemetry/partition-packing/PartitionPerformance.py
@@ -1,18 +1,19 @@
 # This program is a benchmark test for running Spark Jobs on a cluster with different partition 
 # strategies.
 import argparse
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 from moztelemetry.dataset import Dataset
 
-def submit(docType, channel, timestamp):
-    sc = SparkContext("local", "ecole")
+def submit(docType, channel, timestamp, group_by):
+    spark = SparkSession.builder.getOrCreate()
+    sc = spark.sparkContext
     rdd = Dataset.from_source("telemetry"
         ).where(
             docType=docType,
             appUpdateChannel=channel,
             submissionDate=timestamp
-        ).records(sc)
+        ).records(sc, group_by=group_by)
     print("Number of rows of RDD: {}".format(rdd.count()))
     print("Number of partitions used: {}".format(rdd.getNumPartitions()))
     sc.stop()
@@ -22,5 +23,6 @@ if __name__ == '__main__':
     parser.add_argument("docType", type=str)
     parser.add_argument("channel", type=str)
     parser.add_argument("timestamp", type=str)
+    parser.add_argument("group_by", type=str)
     args = parser.parse_args()
-    submit(docType=args.docType, channel=args.channel, timestamp=args.timestamp)
+    submit(docType=args.docType, channel=args.channel, timestamp=args.timestamp, group_by=args.group_by)
